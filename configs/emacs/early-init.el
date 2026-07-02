a2c8a063e809059bc48db9aca88cd40795dbb0cc
Haido <r4cer.cyclist@gmail.com>
2026-07-02 05:02:55 +0300

    Fix startup window size on Windows.

--- configs/emacs/early-init.el
+++ configs/emacs/early-init.el
@@ -1,6 +1,7 @@
 ;;; early-init.el --- Early initialization -*- lexical-binding: t; no-byte-compile: t -*-
 
 (defvar gc-cons-threshold-old gc-cons-threshold)
+(defvar nt? (eq system-type 'windows-nt))
 
 (setq gc-cons-threshold most-positive-fixnum ; old value is 800000
       read-process-output-max (* 1024 1024 4) ; 4mb
@@ -11,14 +12,14 @@
       native-comp-speed 2)
 
 (modify-all-frames-parameters
- '((menu-bar-lines . 0)
+ `((menu-bar-lines . 0)
    (vertical-scroll-bars . nil)
    (tool-bar-lines . 0)
    (width . 128)
-   (height . 68)))
+   (height . ,(if nt? 45 68))))
 
 ;; Fix problem with gpg on Win10
-(when (memq system-type '(windows-nt))
+(when nt?
   (setq package-gnupghome-dir (expand-file-name "gnupg" (getenv "APPDATA"))))
 
 (require 'package)
